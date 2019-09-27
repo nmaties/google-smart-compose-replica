2a97b84a510ec26448a8e5608a55b9081b8afd45
Maties Nicolae <nicolaematies@gmail.com>
2019-09-27 14:41:05 +0100

    Updates

---
+++ precache-manifest.47b1de33b309da9325ffbd01f36f8e46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55c6b996d370e4d9db9a9aee39192be7",
+    "url": "/google-smart-compose-replica/index.html"
+  },
+  {
+    "revision": "4de0db98daf5d9339f1b",
+    "url": "/google-smart-compose-replica/static/css/main.b704a955.chunk.css"
+  },
+  {
+    "revision": "4876f879bc7a1f83391f",
+    "url": "/google-smart-compose-replica/static/js/2.85729427.chunk.js"
+  },
+  {
+    "revision": "4de0db98daf5d9339f1b",
+    "url": "/google-smart-compose-replica/static/js/main.0683a5db.chunk.js"
+  },
+  {
+    "revision": "902635597eead39c1230",
+    "url": "/google-smart-compose-replica/static/js/runtime-main.f73dc109.js"
+  }
+]);
